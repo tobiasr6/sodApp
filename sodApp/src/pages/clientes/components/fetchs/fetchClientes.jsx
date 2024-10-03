89c14b13d77bcbd2a23e2698ad87ddbcdbba8e9b
tobiasr6 <requenatobias3@gmail.com>
2024-10-02 23:44:11 -0300

    feat: Renderizar los clientes.

---
+++ sodApp/src/pages/clientes/components/fetchs/fetchClientes.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import apiConfig from '../../../../services/apiRoutes'; // Asegúrate de importar la configuración de tu API correctamente
+import { message } from 'antd';
+
+const useFetchClientes = () => {
+  const [clientes, setClientes] = useState([]);
+  
+  const fetchClientes = async () => {
+    try {
+      const response = await axios.get(
+        `${apiConfig.API_BASE_URL}${apiConfig.apiClientes.todosClientes}`
+      );
+      const usersArray = Array.isArray(response.data)
+        ? response.data
+        : [];
+      setClientes(usersArray);
+      console.log('Data: ', response.data)
+
+    } catch (error) {
+      console.error('Hubo un error al obtener los clientes:', error);
+      message.error('Error al obtener los clientes');
+    }
+
+  };
+
+  useEffect(() => {
+    fetchClientes();
+  }, []);
+
+  return [clientes, fetchClientes];
+};
+
+export default useFetchClientes;
