eaeb28f71af6bf47c3058a8c1a3bca1a57cbd80f
tobiasr6 <requenatobias3@gmail.com>
2024-10-15 23:58:14 -0300

    change[Cliente]: edicion completa

--- sodApp/src/pages/clientes/components/EditCliente.jsx
+++ sodApp/src/pages/clientes/components/EditCliente.jsx
@@ -16,33 +16,56 @@ const EditarCliente = ({ cliente }) => {
   const [telefono, setTelefono] = useState('');
   const [observaciones, setObservaciones] = useState('');
   const [pedidos, setPedidos] = useState([{ cantidad: '', idProducto: null }]);
-  const [diasRecorrido, setDiasRecorrido] = useState([{ idDia: null }]);
+  const [diasRecorrido, setDiasRecorrido] = useState([{ idDia: '' }]);
+  const [idCliente, setIdCliente] = useState('');
 
   // Hook personalizado para obtener los barrios, días y productos
   const { barrios } = useFetchBarrios();
   const { dias } = useFetchDias();
   const { productos } = useFetchProductos();
 
-  useEffect(() => {
-    // Cargar datos del cliente en los campos al abrir el modal
+  const showModal = () => {
+    setIsModalOpen(true);
+
+    // Inicializamos los valores del cliente al abrir el modal
     if (cliente) {
+      setIdCliente(cliente.id);
       setNombre(cliente.nombre);
       setDireccion(cliente.direccion);
       setSelectedBarrio(cliente.idBarrio);
       setTelefono(cliente.telefono);
       setObservaciones(cliente.observaciones);
-      setPedidos(cliente.pedidos.map(pedido => ({ cantidad: pedido.cantidad, idProducto: pedido.idProducto })));
-      setDiasRecorrido(cliente.diasRecorrido.map(dia => ({ idDia: dia.dia })));
-    }
-  }, [cliente]);
 
-  const showModal = () => {
-    setIsModalOpen(true);
+      setPedidos(
+        cliente.pedidos.map((pedido) => {
+          const productoEncontrado = productos.find(
+            (prod) => prod.tipoProducto === pedido.producto
+          );
+          return {
+            cantidad: pedido.cantidad,
+            idProducto: productoEncontrado
+              ? productoEncontrado.idProducto
+              : null // Asegurarse de encontrar el ID
+          };
+        })
+      );
+
+      setDiasRecorrido(
+        cliente.diasRecorrido.map((diaRecorrido) => {
+          const diaEncontrado = dias.find(
+            (dia) => dia.diaSemana === diaRecorrido.dia
+          );
+          return {
+            idDia: diaEncontrado ? diaEncontrado.idDia : null
+          };
+        })
+      );
+    }
   };
 
   const handleCancel = () => {
     setIsModalOpen(false);
-    resetFields();
+    resetFields(); // Reseteamos los campos al cerrar el modal
   };
 
   const resetFields = () => {
@@ -54,30 +77,36 @@ const EditarCliente = ({ cliente }) => {
     setPedidos([{ cantidad: '', idProducto: null }]);
     setDiasRecorrido([{ idDia: null }]);
   };
-
   const handleOk = async () => {
     const clienteData = {
+      idCliente,
       nombre,
       direccion,
       idBarrio: selectedBarrio,
       telefono,
       observaciones,
-      pedidos: pedidos.map(pedido => ({
+      pedidos: pedidos.map((pedido) => ({
         cantidad: Number(pedido.cantidad),
-        producto: pedido.idProducto,
+        producto: pedido.idProducto
       })),
-      diasRecorrido: diasRecorrido.map(diaRecorrido => ({
-        dia: diaRecorrido.idDia,
-      })),
+      diasRecorrido: diasRecorrido.map((diaRecorrido) => ({
+        dia: diaRecorrido.idDia
+      }))
     };
 
     try {
-      await editClient(cliente.idCliente, clienteData); // Enviar al backend
+      await editClient(cliente.id, clienteData); // Enviar al backend
       message.success('Cliente actualizado con éxito');
       handleCancel();
+      window.location.reload();
+      // console.log('clienteData', clienteData);
     } catch (error) {
       message.error('Error al actualizar el cliente');
+      // console.log('clienteData', clienteData);
+      // console.log('diasRecorrido', diasRecorrido);
+      // console.log('dias', dias);
     }
+
   };
 
   const addPedido = () => {
@@ -106,34 +135,38 @@ const EditarCliente = ({ cliente }) => {
     setDiasRecorrido(newDiasRecorrido);
   };
 
-  const handleDiaRecorridoChange = (index, value) => {
+  const handleDiaRecorridoChange = (index, field, value) => {
     const newDiasRecorrido = [...diasRecorrido];
-    newDiasRecorrido[index].idDia = value;
+    newDiasRecorrido[index][field] = value;
     setDiasRecorrido(newDiasRecorrido);
   };
 
   return (
     <>
       <Button
-        type="primary"
-        icon={<EditOutlined style={{ color: 'black', backgroundColor: 'transparent' }} />}
+        type='primary'
+        icon={
+          <EditOutlined
+            style={{ color: 'black', backgroundColor: 'transparent' }}
+          />
+        }
         onClick={showModal}
         style={{
           backgroundColor: 'transparent',
-          borderColor: 'gray',
+          borderColor: 'gray'
         }}
       />
       <Modal
-        title="Editar Cliente"
+        title='Editar Cliente'
         open={isModalOpen}
         onCancel={handleCancel}
         footer={[
-          <Button key="cancel" onClick={handleCancel}>
+          <Button key='cancel' onClick={handleCancel}>
             Cancelar
           </Button>,
-          <Button key="submit" type="primary" onClick={handleOk}>
+          <Button key='submit' type='primary' onClick={handleOk}>
             Guardar
-          </Button>,
+          </Button>
         ]}
       >
         <Input
@@ -183,30 +216,35 @@ const EditarCliente = ({ cliente }) => {
             <div key={index} style={{ display: 'flex', marginBottom: 10 }}>
               <Input
                 value={pedido.cantidad}
-                onChange={(e) => handlePedidoChange(index, 'cantidad', e.target.value)}
+                onChange={(e) =>
+                  handlePedidoChange(index, 'cantidad', e.target.value)
+                }
                 placeholder='Cantidad'
                 style={{ width: '30%', marginRight: 10 }}
                 required
               />
               <Select
+                value={pedido.idProducto || undefined} // Asegúrate de que el valor inicial esté correctamente definido
                 style={{ width: '60%', marginRight: 10 }}
                 placeholder='Selecciona un producto'
-                onChange={(value) => handlePedidoChange(index, 'idProducto', value)}
-                value={pedido.idProducto}
+                onChange={(value) =>
+                  handlePedidoChange(index, 'idProducto', value)
+                }
                 required
               >
                 {productos.map((producto) => (
                   <Option key={producto.idProducto} value={producto.idProducto}>
-                    {producto.nombreProducto}
+                    {producto.tipoProducto}
                   </Option>
                 ))}
               </Select>
-              <Button onClick={() => removePedido(index)} type="link" danger>
+
+              <Button onClick={() => removePedido(index)} type='link' danger>
                 Eliminar
               </Button>
             </div>
           ))}
-          <Button type="dashed" onClick={addPedido}>
+          <Button type='dashed' onClick={addPedido}>
             Agregar Pedido
           </Button>
         </div>
@@ -216,24 +254,31 @@ const EditarCliente = ({ cliente }) => {
           {diasRecorrido.map((diaRecorrido, index) => (
             <div key={index} style={{ display: 'flex', marginBottom: 10 }}>
               <Select
+                value={diaRecorrido.idDia || undefined} // Asegúrate de que sea el ID numérico
                 style={{ width: '100%', marginRight: 10 }}
                 placeholder='Selecciona un día'
-                onChange={(value) => handleDiaRecorridoChange(index, value)}
-                value={diaRecorrido.idDia}
+                onChange={(value) =>
+                  handleDiaRecorridoChange(index, 'idDia', value)
+                }
                 required
               >
                 {dias.map((dia) => (
                   <Option key={dia.idDia} value={dia.idDia}>
-                    {dia.nombreDia}
+                    {dia.diaSemana}
                   </Option>
                 ))}
               </Select>
-              <Button onClick={() => removeDiaRecorrido(index)} type="link" danger>
+
+              <Button
+                onClick={() => removeDiaRecorrido(index)}
+                type='link'
+                danger
+              >
                 Eliminar
               </Button>
             </div>
           ))}
-          <Button type="dashed" onClick={addDiaRecorrido}>
+          <Button type='dashed' onClick={addDiaRecorrido}>
             Agregar Día de Recorrido
           </Button>
         </div>
