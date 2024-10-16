a6082229d9fa9fadd55cd3c1d84c4a00ce7e9364
tobiasr6 <requenatobias3@gmail.com>
2024-10-15 23:56:13 -0300

    fix[cliente]: No envia el clienteData

--- sodApp/src/pages/clientes/services/addCliente.jsx
+++ sodApp/src/pages/clientes/services/addCliente.jsx
@@ -3,9 +3,14 @@ import apiConfig from '../../../services/apiRoutes';
 
 export const agregarClienteService = async (clienteData) => {
   try {
-    const response = await axios.post(`${apiConfig.API_BASE_URL}${apiConfig.apiClientes.crearCliente}`);
+    const response = await axios.post(
+      `${apiConfig.API_BASE_URL}${apiConfig.apiClientes.crearCliente}`,
+      clienteData // Aquí se pasa el objeto clienteData en el cuerpo de la solicitud
+    );
+    console.log('Response data', response.data);
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.error || 'Error al agregar cliente');
   }
 };
+
