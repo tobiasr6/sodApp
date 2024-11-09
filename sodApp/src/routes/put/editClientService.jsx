808d3c0d2a81bc95997b51607ce876d869a96cb2
tobiasr6 <requenatobias3@gmail.com>
2024-11-09 01:14:12 -0300

    change: Agregado servide para editar el estado del clliente.

--- sodApp/src/routes/put/editClientService.jsx
+++ sodApp/src/routes/put/editClientService.jsx
@@ -5,3 +5,8 @@ export const editClient = async (idCliente, clienteData) => {
   const response = await axios.put(`${apiConfig.API_BASE_URL}${apiConfig.apiClientes.editarCliente(idCliente)}`, clienteData);
   return response.data;
 };
+
+export const editEstadoClient = async (idCliente, clienteEstado) => {
+  const response = await axios.patch(`${apiConfig.API_BASE_URL}${apiConfig.apiClientes.modificarEstado(idCliente)}`, clienteEstado)
+  return response.data;
+}
